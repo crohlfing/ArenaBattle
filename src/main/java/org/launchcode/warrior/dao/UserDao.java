f1d0896aa822918ef96b8af41cb0908e8fd85eaf
131-rohlfing <crohl_000@Matilda.attlocal.net>
2017-01-30 15:42:48 -0600

    message

---
+++ src/main/java/org/launchcode/warrior/dao/UserDao.java
@@ -0,0 +1,23 @@
+package org.launchcode.warrior.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.launchcode.warrior.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Transactional
+@Repository
+public interface UserDao extends CrudRepository<User, Integer> {
+
+    User findByUid(int uid);
+    
+    User findByUsername(String username);
+    
+    List<User> findAll();
+    
+    // TODO - add method signatures as needed
+
+}
